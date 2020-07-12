47367492cc485c3cb64d793d9d2f106cf2c8874a
lioneagle <leoeagle@163.com>
2020-07-12 15:04:06 +0800

    add basic type SetString

---
+++ types/set.go
@@ -0,0 +1,64 @@
+package types
+
+import (
+	"sort"
+
+	"github.com/pkg/errors"
+)
+
+type SetString struct {
+	data []string
+}
+
+func NewSetString() *SetString {
+	return &SetString{
+		data: make([]string, 0),
+	}
+}
+
+func (this *SetString) Len() int {
+	return len(this.data)
+}
+
+func (this *SetString) Add(items ...string) {
+	for _, v := range items {
+		if !this.Find(v) {
+			this.data = append(this.data, v)
+		}
+	}
+}
+
+func (this *SetString) Find(item string) bool {
+	for _, v := range this.data {
+		if item == v {
+			return true
+		}
+	}
+	return false
+}
+
+func (this *SetString) Foreach(f func(index int, item string) (stop bool, err error)) error {
+	if f == nil {
+		return errors.Errorf("f is nil")
+	}
+
+	for i, v := range this.data {
+		stop, err := f(i, v)
+		if err != nil {
+			return err
+		}
+		if stop {
+			return nil
+		}
+	}
+	return nil
+}
+
+func (this *SetString) Sort() {
+	sort.Slice(this.data, func(i, j int) bool {
+		if len(this.data[i]) < len(this.data[j]) {
+			return true
+		}
+		return this.data[i] < this.data[j]
+	})
+}
---
+++ types/set_test.go
@@ -0,0 +1,110 @@
+package types
+
+import (
+	"fmt"
+	//"net/http"
+	"testing"
+
+	"github.com/pkg/errors"
+
+	"github.com/lioneagle/goutil/src/test"
+)
+
+func TestSetStringAdd(t *testing.T) {
+	testdata := []struct {
+		items  []string
+		wanted []string
+	}{
+		{[]string{}, []string{}},
+		{[]string{"a"}, []string{"a"}},
+		{[]string{"a", "b"}, []string{"a", "b"}},
+		{[]string{"a", "b", "a"}, []string{"a", "b"}},
+	}
+
+	for i, v := range testdata {
+		v := v
+
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			t.Parallel()
+
+			set := NewSetString()
+			set.Add(v.items...)
+
+			test.EXPECT_EQ(t, set.Len(), len(v.wanted), "")
+			test.EXPECT_EQ(t, set.data, v.wanted, "")
+		})
+	}
+}
+
+func TestSetStringForeach(t *testing.T) {
+	items := []string{"a", "b", "c"}
+
+	set := NewSetString()
+	set.Add(items...)
+
+	count := 0
+	checkFunc1 := func(index int, item string) (stop bool, err error) {
+		count++
+		return false, nil
+	}
+
+	checkFunc2 := func(index int, item string) (stop bool, err error) {
+		count++
+		if index >= 1 {
+			return true, nil
+		}
+		return false, nil
+	}
+
+	checkFunc3 := func(index int, item string) (stop bool, err error) {
+		count++
+		if index >= 2 {
+			return false, errors.Errorf("test err")
+		}
+		return false, nil
+	}
+
+	count = 0
+	err := set.Foreach(nil)
+	test.EXPECT_TRUE(t, err != nil, "")
+
+	count = 0
+	err = set.Foreach(checkFunc1)
+	test.EXPECT_TRUE(t, err == nil, "err = %+v", err)
+	test.EXPECT_EQ(t, count, len(items), "")
+
+	count = 0
+	err = set.Foreach(checkFunc2)
+	test.EXPECT_TRUE(t, err == nil, "err = %+v", err)
+	test.EXPECT_EQ(t, count, 2, "")
+
+	count = 0
+	err = set.Foreach(checkFunc3)
+	test.EXPECT_TRUE(t, err != nil, "")
+	test.EXPECT_EQ(t, count, 3, "")
+}
+
+func TestSetStringSort(t *testing.T) {
+	testdata := []struct {
+		items  []string
+		wanted []string
+	}{
+		{[]string{"a", "c", "b"}, []string{"a", "b", "c"}},
+		{[]string{"ab", "c", "b"}, []string{"b", "c", "ab"}},
+		{[]string{"a", "c", "b", "f", "e"}, []string{"a", "b", "c", "e", "f"}},
+	}
+
+	for i, v := range testdata {
+		v := v
+
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			t.Parallel()
+
+			set := NewSetString()
+			set.Add(v.items...)
+			set.Sort()
+
+			test.EXPECT_EQ(t, set.data, v.wanted, "")
+		})
+	}
+}
